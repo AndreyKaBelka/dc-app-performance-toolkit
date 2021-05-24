dc8f6738702f8bc5e4a7d7acff7f0d1539d815ed
Andrey Ovchinnikov <andrey.ovchinnikov@smartforce.io>
2021-05-24 13:43:36 +0600

    finished with selenium tests

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -1,5 +1,7 @@
+import random
+
 from selenium_ui.conftest import print_timing
-from selenium_ui.jira.pages.pages import Issue, IntercomIssue
+from selenium_ui.jira.pages.pages import IntercomIssue
 from selenium_ui.jira.pages.selectors import IntercomSelectors
 
 
@@ -34,6 +36,65 @@ def intercom_add_link_to_issue_action(webdriver, datasets):
             intercom_issue_page.wait_until_clickable(IntercomSelectors.add_link_button).click()
             intercom_issue_page.wait_until_clickable(IntercomSelectors.link_input)
             intercom_issue_page.get_element(IntercomSelectors.link_input).send_keys(con_url)
+
+        sub_measure()
+
+    measure()
+
+
+def intercom_chat_load_action(webdriver, datasets):
+    intercom_issue_page = IntercomIssue(webdriver, issue_key=datasets['intercom_issue'][0])
+
+    @print_timing('selenium_intercom_chat_load')
+    def measure():
+        @print_timing('selenium_intercom_chat_load:load_issue')
+        def sub_measure():
+            intercom_issue_page.go_to()
+            intercom_issue_page.wait_for_page_loaded()
+            intercom_issue_page.wait_until_visible(IntercomSelectors.conversation_button)
+
+        sub_measure()
+
+        @print_timing('selenium_intercom_chat_load:click_chat_button')
+        def sub_measure():
+            chat_elements = intercom_issue_page.get_elements(IntercomSelectors.conversation_button)
+            random.choice(chat_elements).click()
+
+        sub_measure()
+
+        @print_timing('selenium_intercom_chat_load:wait_until_chat_load')
+        def sub_measure():
+            intercom_issue_page.wait_until_visible(IntercomSelectors.chat_selector)
+
+        sub_measure()
+
+    measure()
+
+
+def intercom_chat_information_load_action(webdriver, datasets):
+    intercom_issue_page = IntercomIssue(webdriver, issue_key=datasets['intercom_issue'][0])
+
+    @print_timing('selenium_intercom_chat_information_load_action')
+    def measure():
+        @print_timing('selenium_intercom_chat_information_load_action:load_issue')
+        def sub_measure():
+            intercom_issue_page.go_to()
+            intercom_issue_page.wait_for_page_loaded()
+            intercom_issue_page.wait_until_visible(IntercomSelectors.information_button)
+
+        sub_measure()
+
+        @print_timing('selenium_intercom_chat_information_load_action:click_chat_information_button')
+        def sub_measure():
+            chat_elements = intercom_issue_page.get_elements(IntercomSelectors.information_button)
+            random.choice(chat_elements).click()
+
+        sub_measure()
+
+        @print_timing('selenium_intercom_chat_information_load_action:wait_until_chat_information_load')
+        def sub_measure():
+            intercom_issue_page.wait_until_present(IntercomSelectors.all_information_inside_selector)
+
         sub_measure()
 
     measure()
